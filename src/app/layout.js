3890874fc494a655563ddb6b5a9262b1bb0d9e09
Harish <harish9499428306@gmail.com>
2025-04-03 22:46:21 +0530

    news

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { Header } from "@components/header";
-import { Footer } from "@components/footer";
+// import { Header } from "@components/header";
+// import { Footer } from "@components/footer";
 import Head from "next/head";
 
 const geistSans = Geist({
